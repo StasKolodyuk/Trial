35899b6fee255aa9e71d20879805d95e4ceeb637
Aspire <Aspire@Aspire-PC>
2014-07-15 15:27:57 +0300

    first serious commit

--- src/first/Main.java
+++ src/first/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This project should be at GitHub");
+        System.out.println("First serious commit");
     }
     
 }
